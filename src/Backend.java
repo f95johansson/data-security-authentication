7a0d8568e708190de2d75de5b2078d79cf60010f
Oskar Olausson <id14oon@cs.umu.se>
2018-11-01 08:36:02 +0100

    Better test setup, no need to start sever independently

--- src/Backend.java
+++ src/Backend.java
@@ -3,14 +3,18 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
 public class Backend {
+
+  public static void StartServer() {
+      try {
+          Registry registry = LocateRegistry.createRegistry(8099);
+          registry.rebind("printer", new PrinterService());
+          System.out.println("Server started :)");
+      } catch(RemoteException e) {
+          System.out.println("Error while creating the server");
+      }
+  }
   
   public static void main(String args[]) {
-      try {
-        Registry registry = LocateRegistry.createRegistry(8099);
-        registry.rebind("printer", new PrinterService());
-        System.out.println("Server started :)");
-      } catch(RemoteException e) {
-        System.out.println("Erroor while creating the server");
-      }
+      StartServer();
   }
 }
--- test/ClientTest.java
+++ test/ClientTest.java
@@ -1,4 +1,4 @@
-import org.junit.Assert;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -15,12 +15,18 @@ public class ClientTest {
 
     @BeforeClass
     public static void SetPrinter() {
+        Backend.StartServer();
+
         try {
             rmiPrinter = (RMIPrinter) Naming.lookup("rmi://localhost:8099/printer");
         } catch (NotBoundException | MalformedURLException | RemoteException e) {
-            System.err.println("Did you remember to start the server");
-            System.exit(1);
+            e.printStackTrace();
         }
+    }
+
+    @Before
+    public void Reset() throws RemoteException {
+        rmiPrinter.restart();
     }
 
     @Test
@@ -30,5 +36,19 @@ public class ClientTest {
 
         rmiPrinter.setConfig(settingsName, value);
         assertEquals(value, rmiPrinter.readConfig(settingsName));
+    }
+
+    @Test
+    public void should_return_all_jobs() throws RemoteException {
+        final String fileName = "elvis_still_alive_proof.png";
+        final String fileName2 = "ufo_proof.png";
+        final String printerName = "some_printer";
+
+        rmiPrinter.print(fileName, printerName);
+        rmiPrinter.print(fileName2, printerName);
+
+        String[] queue = rmiPrinter.queue().split("\n");
+        assertTrue(queue[0].contains(fileName));
+        assertTrue(queue[1].contains(fileName2));
     }
 }
