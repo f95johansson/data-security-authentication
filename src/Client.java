a804e7427a542c58c7b8dc53c5ff3c4c5a12577a
Oskar Olausson <id14oon@cs.umu.se>
2018-11-13 18:52:03 +0100

    removed hårdkodat

--- src/Client.java
+++ src/Client.java
@@ -5,16 +5,6 @@ import java.rmi.NotBoundException;
 
 public class Client {
 
-    public static final String DEFAULT_USERNAME = "Cecilia";
-    public static final String DEFAULT_PASSWORD = "Passw0rd123";
-
-    private static void clearPasswords() {
-        //this only works since they are sharing the same files, impossible to do remotely
-        UserRegistration userRegistration = new UserRegistration(new Users());
-        userRegistration.burnThePlace();
-        userRegistration.addUser(DEFAULT_USERNAME, DEFAULT_PASSWORD);
-    }
-
     public static RMIPrinter startClient() throws RemoteException, NotBoundException, MalformedURLException {
         return startClient(8099);
     }
@@ -24,18 +14,8 @@ public class Client {
     }
 
     public static void main(String[] args) throws RemoteException, NotBoundException, MalformedURLException {
-        clearPasswords();
         RMIPrinter printer = startClient();
-        String sessionKey = printer.logIn(DEFAULT_USERNAME, DEFAULT_PASSWORD);
-
-        printer.setConfig("magenta", "232", sessionKey);
-        printer.readConfig("magenta", sessionKey);
-        printer.status(sessionKey);
-        printer.stop(sessionKey);
-        printer.start(sessionKey);
-        printer.print("unimportant", "dtu-808-printer", sessionKey);
-        int job = printer.print("DTU.txt", "dtu-808-printer", sessionKey);
-        printer.topQueue(job, sessionKey);
-        printer.queue(sessionKey);
+        String sessionKey = printer.logIn("Cecilia", "Passw0rd123");
+        printer.print("print.txt", "the-big-one", sessionKey);
     }
 } 
